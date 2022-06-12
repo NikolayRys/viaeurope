0362142e542825dc7a301d03fb2acb466b9e8b0a
Nikolay Rys <nikolay@rys.me>
2022-06-13 01:29:40 +0300

    Add fitting check

---
+++ distibute.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+require 'pry-byebug'
+require 'csv'
+
+# Read file
+raw_parcels_csv = File.read('input.csv')
+parcels = CSV.parse(csv_file, headers: true)
+
+
+
+# Combine parcels
+
+# Try heuristics
+
+# Try brute force
+
+# Invert bins by items
+
+# Print result
