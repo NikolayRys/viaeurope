387cfe5d43d71faab9c49b80017006e69eb42d75
Nikolay Rys <nikolay@rys.me>
2022-06-13 13:30:17 +0300

    Move algorithm to their own directories

---
+++ lib/algos/bin_completion.rb
@@ -0,0 +1,20 @@
+# Modern algorithm for exhaustive search
+# Paper: https://www.aaai.org/Papers/AAAI/2002/AAAI02-110.pdf
+
+class BinCompletion
+  def initialize(bins, size=Bin::SIZE)
+    @virtual_bins = bins
+    @size = size
+  end
+
+  def distribute!
+
+  end
+
+  private
+
+  def calculate_lower_bound
+
+  end
+
+end
--- spec/algos/best_fit_decreasing_spec.rb
+++ spec/algos/best_fit_decreasing_spec.rb
@@ -1,5 +1,5 @@
 require_relative 'spec_helper'
-require_relative '../lib/best_fit_decreasing'
+require_relative '../../lib/algos/best_fit_decreasing'
 
 describe BestFitDecreasing do
 
@@ -16,7 +16,7 @@ describe BestFitDecreasing do
   test_data_table << [[100, 98, 96, 93, 91, 87, 81, 59, 58, 55, 50, 43, 22, 21, 20, 15, 14, 10, 8, 6, 5, 4, 3, 1, 0],
                       [[93, 6, 1, 0], [87, 10, 3], [59, 22, 15, 4], [81, 14, 5], [100], [91, 8], [58, 21, 20], [55, 43], [98], [96], [50]]]
   test_data_table << [[51, 48, 40, 26, 26, 9],
-                      [[51, 48], [40, 26, 26], [9]]] # Algorithm cannot find optimal solution for this case
+                      [[51, 48], [40, 26, 26], [9]]] # BFD cannot find optimal solution for this case
 
   test_data_table.each do|values, bins|
     it "distributes set of #{values.size} values into bins" do
