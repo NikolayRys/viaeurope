227ccdf31812c7b96a66c74dc5222b627e135065
Nikolay Rys <nikolay@rys.me>
2022-06-15 22:50:11 +0300

    Refine brute force implementation

--- lib/algos/brute_force.rb
+++ lib/algos/brute_force.rb
@@ -1,6 +1,4 @@
 # Brute force with some branch-pruning
-# We can add the number of bins completed to the sum of the remaining elements, divided by the bin capacity and rounded up.
-
 
 class BruteForce
   def initialize(values, lower_bound=0, size=Bin::SIZE)
@@ -10,45 +8,33 @@ class BruteForce
   end
 
   def distribute!
-
+    find_distribution(@values, @lower_bound, @size)
   end
 
   private
 
-
   def find_distribution(all_items, bins, size)
     return [] if all_items.empty?
 
-    # If one bin left, put all items in it
-    min_items = bins == 1 ? all_items.size : 1
-
-    # Limit the items to make sure there are no any empty bins
-    max_items = all_items.size - bins + 1
+    min_items = bins == 1 ? all_items.size : 1 # If one bin left, put all items in it
+    max_items = all_items.size - bins + 1 # Limit the items to make sure there are no any empty bins
 
     core_item = all_items.first
-    core_bin_variants = []
 
-    (min_items..max_items).each do |number_of_items|
-      core_bin_variants.concat(all_items.drop(1).combination(number_of_items - 1).reject{|values| core_item + values.sum > size })
-    end
-
-    # Enumerator.new { |y|
-    #   (min_items..max_items).each do |number_of_items|
-    #     all_items.drop(1).combination(number_of_items - 1).each{|enum| enum.each { |i| y << i } }
-    #   end
-    # }.reject{|values| core_item + values.sum > size }
-
-
-    core_bin_variants.map! { |values| [core_item] + values }
-
-    core_bin_variants.each do |core_bin|
-      other_bins = find_distribution(all_items - core_bin, bins - 1, size)
-
-      if other_bins && ([core_bin] + other_bins).flatten.size == all_items.size
-        return [core_bin] + other_bins
+    core_bin_variants = Enumerator.new do |y| # Use Enumerator to not instantiate the array in memory
+      (min_items..max_items).each do |items_in_bin|
+        all_items.drop(1).combination(items_in_bin - 1).each { |c| y << c.unshift(core_item) }
       end
     end
-  end
 
+    core_bin_variants.each do |core_bin|
+      next if core_bin.sum > size
+
+      other_bins = find_distribution(all_items - core_bin, bins - 1, size)
+      return other_bins.unshift(core_bin) if other_bins
+    end
+
+    nil
+  end
 
 end
