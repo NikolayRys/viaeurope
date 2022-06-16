a5e25bf783be881a73a6222c861b2f6fa878f0a0
Nikolay Rys <nikolay@rys.me>
2022-06-16 03:57:13 +0300

    Provide filename defaults

--- assign_shipments.rb
+++ assign_shipments.rb
@@ -5,10 +5,9 @@ require_relative 'lib/algos/brute_force'
 require_relative 'lib/lower_bound'
 
 time_budget = ARGV[2].to_i || 3
-fp = FileProcessor.new(ARGV[0])
+fp = FileProcessor.new(ARGV[0] || 'input.csv')
 fp.read_file!
 values = fp.get_values
-
 
 optimal_packing = LowerBound.new(values).get_min_bins
 shipments = BestFitDecreasing.new(values).distribute!
@@ -23,6 +22,5 @@ if optimal_packing < shipments.size && time_budget > 0
   end
 end
 
-
 fp.set_shipments(shipments)
-fp.write_file!(ARGV[1])
+fp.write_file!(ARGV[1] || 'output.csv')
